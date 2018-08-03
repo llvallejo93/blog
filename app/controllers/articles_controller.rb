0b593c1d93dc4695ec1ee343b9348b1b20259362
llvallejo93 <llvallejo93@gmail.com>
2018-08-03 16:36:32 -0500

    Bootstrap & CSS
    Started adding more visual features to the application using the Bootstrap gem and CSS.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,7 +19,7 @@ class ArticlesController < ApplicationController
       @article = Article.new(article_params)
    
       if @article.save
-        redirect_to @article
+        redirect_to @article, notice: "The article has been created!"
       else
         render 'new'
       end
@@ -39,7 +39,7 @@ class ArticlesController < ApplicationController
       @article = Article.find(params[:id])
       @article.destroy
    
-      redirect_to articles_path
+      redirect_to articles_path, notice: "The post has successfully deleted."
     end
    
     private
